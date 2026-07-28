ed5ea745e9c22919a869feb243c34a5083df1ff1
synthoria <nukehelicopter123@gmail.com>
2026-07-28 12:22:13 +0500

    update api

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import datetime
 import os
+from zoneinfo import ZoneInfo
 
 from fastapi import FastAPI, status
 from aiogram import Bot
@@ -10,6 +11,32 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI()
+
+WEEKDAYS = (
+    "понедельник",
+    "вторник",
+    "среду",
+    "четверг",
+    "пятницу",
+    "субботу",
+    "воскресенье",
+)
+
+MONTHS = (
+    "",
+    "января",
+    "февраля",
+    "марта",
+    "апреля",
+    "мая",
+    "июня",
+    "июля",
+    "августа",
+    "сентября",
+    "октября",
+    "ноября",
+    "декабря",
+)
 
 class NotificationMessage(BaseModel):
     notify_at: datetime.datetime
@@ -23,6 +50,23 @@ class NotificationMessage(BaseModel):
 class BotMessage:
     message: str
     date: datetime.datetime
+
+    def formatted_date(self) -> str:
+        local_date = self.date.astimezone(
+            ZoneInfo("Asia/Yekaterinburg")
+        )
+
+        weekday = WEEKDAYS[local_date.weekday()]
+        month = MONTHS[local_date.month]
+
+        return (
+            f"в {weekday}, "
+            f"{local_date.day} {month}, "
+            f"в {local_date:%H:%M}"
+        )
+
+    def text(self) -> str:
+        return f"{self.message} {self.formatted_date()}"
     
 
 @app.post("/send_notification")
@@ -30,11 +74,11 @@ async def send_notification(message: NotificationMessage):
     # print(message)
     # print(message.notify_at)
     
-    bot_message = BotMessage(message=f"Встреча составлена, она будет в {message.notify_at}", date=message.notify_at)
+    bot_message = BotMessage(message=f"{os.getenv("BOT_MESSAGE")} {message.notify_at}", date=message.notify_at)
     
     try:
         async with Bot(token=os.getenv("TELEGRAM_TOKEN")) as bot:
-            await bot.send_message(os.getenv("MY_CHAT_ID"), text=bot_message.message)
+            await bot.send_message(os.getenv("MY_CHAT_ID"), text=bot_message.text())
         return {"status": status.HTTP_200_OK,
                 "message": "notification sended"
                 }
